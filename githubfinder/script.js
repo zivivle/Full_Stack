4dfbbf03a7d6aa67fbdc2f8c5fef63a4b01c3b35
zivivle <zivivle@gmail.com>
2023-08-29 21:40:17 +0900

    fix: OOP로 최적화하여 코드 수정

--- githubfinder/script.js
+++ githubfinder/script.js
@@ -2,246 +2,212 @@ const auth1 = 'ghp_d2nCYUB';
 const auth2 = 'fcgWdcbmg';
 const auth3 = '90BiQfmWbCy';
 const auth4 = '3Rn1Gh8Ah';
-const searchInput = document.querySelector('.searchInput');
-const ghchartForm = document.getElementsByClassName('ghchart');
-const spinnerIcon = document.getElementById('spinner');
+class GithubUser {
+  constructor() {
+    this.searchInput = document.querySelector('.searchInput');
+    this.spinnerIcon = document.getElementById('spinner');
+    this.ghchartForm = document.getElementsByClassName('ghchart')[0];
 
-//깃허브 유저 데이터 가져오는 비동기 함수
-function fetchGithubUserInfo(token, searchValue) {
-  //로딩시 spinner icon을 보여줌
-  spinnerIcon.style.display = 'block';
+    this.initEventListeners();
+  }
 
-  fetch(`https://api.github.com/users/${searchValue}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: 'token ' + token,
-    },
-  })
-    .then((response) => {
-      if (!response.ok && response.status === 404) {
-        throw new Error('User not found');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      displayGithubUserInfo(data);
-      spinnerIcon.style.display = 'none';
-    })
-    .catch((error) => {
-      if (error.message === 'User not found') {
-        emptyData();
-        spinnerIcon.style.display = 'none';
+  initEventListeners() {
+    this.searchInput.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter' && this.searchInput.value) {
+        const searchValue = this.searchInput.value.trim();
+        this.fetchUserInfo(auth1 + auth2 + auth3 + auth4, searchValue);
+        this.fetchUserRepos(auth1 + auth2 + auth3 + auth4, searchValue);
+        document.getElementById('repoContainer').innerHTML = '';
+        this.searchInput.value = '';
       }
     });
-}
-
-//검색 인풋창에서 엔터를 눌렀을 때 실행되는 이벤트들
-searchInput.addEventListener('keypress', function (e) {
-  if (e.key === 'Enter') {
-    if (searchInput.value === '') return;
-    let searchValue = searchInput.value;
-    fetchGithubUserInfo(auth1 + auth2 + auth3 + auth4, searchValue);
-
-    const repoContainer = document.getElementById('repoContainer');
-    repoContainer.innerHTML = '';
-    fetchGithubUserRepos(auth1 + auth2 + auth3 + auth4, searchValue);
-    searchInput.value = '';
-  }
-});
-
-// 사용자 정보를 화면에 표시하는 함수
-function displayGithubUserInfo(data) {
-  // 데이터 추출
-  let userInfoTags = tagsUserInfo(data);
-  let userInfoDetails = userInfoDetail(data);
-  // UI 업데이트
-  updateUserTags(userInfoTags);
-  updateUserDetails(userInfoDetails);
-  updateUserGhchart(data.login);
-
-  // emptye 데이터를 처리하고 난 뒤 다시 검색했을 때를 작동할 코드
-  const profileBox = document.getElementsByClassName('profileBox');
-  const subTitle = document.getElementsByClassName('subTitle');
-  const repoBox = document.getElementById('repoContainer');
-
-  profileBox[0].style.display = '';
-  subTitle[0].style.display = '';
-  repoBox.style.display = '';
-
-  const errorBox = document.getElementById('errorBox');
-  errorBox.style.display = 'none';
-}
-
-function updateUserGhchart(id) {
-  console.log('id임', id);
-  // 1. ghchart div에 class hidden 삭제
-
-  const ghchartImage = document.getElementById('ghchartImg');
-
-  ghchartForm[0].classList.remove('hidden');
-  ghchartImage.src = `https://ghchart.rshah.org/${id}`;
-
-  // 2. ghchartImg img에 src id 업데이트
-}
-
-//데이터가 없을때 보여줄 데이터를 전달하는 함수
-function emptyData() {
-  let defaultData = defaultUserInfo();
-  updateEmptyData(defaultData);
-}
-
-// 데이터가 없을 때 보여줄 메세지 객체를 리턴하는 함수
-function defaultUserInfo() {
-  return {
-    message: `🤔 "${searchInput.value}" is not registered`,
-  };
-}
-
-// 깃헙 API 응답으로부터 필요한 사용자 정보를 추출하는 함수들
-function tagsUserInfo(data) {
-  return {
-    image: data.avatar_url,
-    publicRepos: data.public_repos,
-    publicGists: data.public_gists,
-    followers: data.followers,
-    following: data.following,
-  };
-}
-
-function userInfoDetail(data) {
-  return {
-    company: data.company,
-    webBlog: data.blog,
-    location: data.location,
-    memberSince: data.created_at,
-  };
-}
-
-// 데이터가 없을때 UI를 업데이트하는 함수
-function updateEmptyData(data) {
-  const profileBox = document.getElementsByClassName('profileBox');
-  const subTitle = document.getElementsByClassName('subTitle');
-  const repoBox = document.getElementById('repoContainer');
-  const errorBox = document.getElementById('errorBox');
-
-  profileBox[0].style.display = 'none';
-  subTitle[0].style.display = 'none';
-  repoBox.style.display = 'none';
-  ghchartForm[0].classList.add('hidden');
-
-  errorBox.innerHTML = data.message; // 수정된 부분
-  errorBox.style.display = ''; // 수정된 부분
-}
-
-// 추출된 사용자 정보로 UI를 업데이트하는 함수들
-function updateUserTags(userInfo) {
-  const userImage = document.getElementById('userImg');
-  const userPublicRepos = document.getElementsByClassName('tag1');
-  const userPublicGists = document.getElementsByClassName('tag2');
-  const userFollowers = document.getElementsByClassName('tag3');
-  const userFollowing = document.getElementsByClassName('tag4');
-
-  userImage.src = userInfo.image;
-  userPublicRepos[0].innerHTML = `Public Repos: ${userInfo.publicRepos}`;
-  userPublicGists[0].innerHTML = `Public Gists: ${userInfo.publicGists}`;
-  userFollowers[0].innerHTML = `Followers: ${userInfo.followers}`;
-  userFollowing[0].innerHTML = `Following: ${userInfo.following}`;
-}
-
-function updateUserDetails(userInfo) {
-  const userCompany = document.getElementsByClassName('info1');
-  const userWebBlog = document.getElementsByClassName('info2');
-  const userLocation = document.getElementsByClassName('info3');
-  const userMemberSince = document.getElementsByClassName('info4');
-
-  if (userInfo.company) {
-    userCompany[0].innerHTML = `Company: ${userInfo.company}`;
-  } else {
-    userCompany[0].innerHTML = '<p>Company: 등록된 정보가 없습니다.</p>';
   }
 
-  if (userInfo.webBlog) {
-    userWebBlog[0].innerHTML = `Website & Blog: <a href="${userInfo.webBlog}" target="_blank">${userInfo.webBlog}</a>`;
-  } else {
-    userWebBlog[0].innerHTML = '<p>Website & Blog: 등록된 정보가 없습니다.</p>';
-  }
+  async fetchUserInfo(token, searchValue) {
+    try {
+      this.showSpinner();
 
-  if (userInfo.location) {
-    userLocation[0].innerHTML = `Location: ${userInfo.location}`;
-  } else {
-    userLocation[0].innerHTML = '<p>Location: 등록된 정보가 없습니다.</p>';
-  }
-
-  userMemberSince[0].innerHTML = `Member Since: ${
-    userInfo.memberSince ? new Date(Date.parse(userInfo.memberSince)).toDateString() : 'Unknown'
-  }`;
-}
-
-//검색된 사용자의 리포지토리 데이터를 가져오는 비동기 함수
-function fetchGithubUserRepos(token, useId) {
-  spinnerIcon.style.display = 'block';
-
-  fetch(`https://api.github.com/users/${useId}/repos`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: 'token ' + token,
-    },
-  })
-    .then((response) => {
-      if (!response.ok && response.status === 404) {
-        throw new Error('User not found');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      spinnerIcon.style.display = 'none';
-      data.forEach((repoData) => {
-        let userRepoInfos = userRepoInfo(repoData);
-        updateUserRepo(userRepoInfos);
+      const response = await fetch(`https://api.github.com/users/${searchValue}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'token ' + token,
+        },
       });
-    })
-    .catch((error) => {
-      spinnerIcon.style.display = 'none';
-      console.error(error);
-    });
-}
 
-// 깃헙 API 응답으로부터 필요한 리퍼지토리 정보를 추출하는 함수
-function userRepoInfo(data) {
-  return {
-    repoNames: data.name,
-    repoStars: data.stargazers_count,
-    repoWatchers: data.watchers_count,
-    repoForks: data.forks_count,
-  };
-}
+      if (!response.ok && response.status === 404) {
+        throw new Error('User not found');
+      }
 
-// 추출한 데이터를 화면에 업데이트하는 함수
-function displayGithubReposInfo(data) {
-  let userRepoInfos = userRepoInfo(data);
-  updateUserRepo(userRepoInfos); //UI 업데이트
-}
-
-// 데이터를 전달받아 UI 업데이트
-function updateUserRepo(repoInfos) {
-  const repoContainer = document.getElementById('repoContainer');
-
-  if (!repoContainer) {
-    return; // repoContainer가 없으면 함수 종료
+      const data = await response.json();
+      this.displayUserInfo(data);
+    } catch (error) {
+      if (error.message === 'User not found') {
+        this.showDefaultUserInfo();
+      }
+    } finally {
+      this.hideSpinner();
+    }
   }
 
-  let template = `
-  <div class="repoBox borderBox flexRow">
+  async fetchUserRepos(token, userId) {
+    try {
+      this.showSpinner();
+
+      const response = await fetch(`https://api.github.com/users/${userId}/repos`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'token ' + token,
+        },
+      });
+
+      if (!response.ok && response.status === 404) {
+        throw new Error('User not found');
+      }
+
+      const data = await response.json();
+      data.forEach((repoData) => {
+        const userRepoInfos = this.extractRepoInfo(repoData);
+        this.updateUserRepo(userRepoInfos);
+      });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.hideSpinner();
+    }
+  }
+
+  displayUserInfo(data) {
+    const userInfoTags = this.extractUserInfo(data);
+    const userInfoDetails = this.extractUserInfoDetail(data);
+
+    this.updateUserTags(userInfoTags);
+    this.updateUserDetails(userInfoDetails);
+    this.updateUserGhchart(data.login);
+
+    const elementsToUpdate = [
+      document.getElementsByClassName('profileBox')[0],
+      document.getElementsByClassName('subTitle')[0],
+      document.getElementById('repoContainer'),
+    ];
+
+    elementsToUpdate.forEach((el) => (el.style.display = ''));
+
+    document.getElementById('errorBox').style.display = 'none';
+  }
+
+  showDefaultUserInfo() {
+    const message = `🤔 "${this.searchInput.value}" is not registered`;
+    const errorBox = document.getElementById('errorBox');
+
+    const elementsToHide = [
+      document.getElementsByClassName('profileBox')[0],
+      document.getElementsByClassName('subTitle')[0],
+      document.getElementById('repoContainer'),
+      this.ghchartForm,
+    ];
+
+    elementsToHide.forEach((el) => (el.style.display = 'none'));
+    this.ghchartForm.classList.add('hidden');
+
+    errorBox.innerHTML = message;
+    errorBox.style.display = '';
+  }
+
+  showSpinner() {
+    this.spinnerIcon.style.display = 'block';
+  }
+
+  hideSpinner() {
+    this.spinnerIcon.style.display = 'none';
+  }
+
+  extractUserInfo(data) {
+    return {
+      image: data.avatar_url,
+      publicRepos: data.public_repos,
+      publicGists: data.public_gists,
+      followers: data.followers,
+      following: data.following,
+    };
+  }
+
+  extractUserInfoDetail(data) {
+    return {
+      company: data.company,
+      webBlog: data.blog,
+      location: data.location,
+      memberSince: data.created_at,
+    };
+  }
+
+  extractRepoInfo(data) {
+    return {
+      repoNames: data.name,
+      repoStars: data.stargazers_count,
+      repoWatchers: data.watchers_count,
+      repoForks: data.forks_count,
+    };
+  }
+
+  updateUserTags(userInfo) {
+    const userImage = document.getElementById('userImg');
+    const userPublicRepos = document.getElementsByClassName('tag1')[0];
+    const userPublicGists = document.getElementsByClassName('tag2')[0];
+    const userFollowers = document.getElementsByClassName('tag3')[0];
+    const userFollowing = document.getElementsByClassName('tag4')[0];
+
+    userImage.src = userInfo.image;
+    userPublicRepos.innerHTML = `Public Repos: ${userInfo.publicRepos}`;
+    userPublicGists.innerHTML = `Public Gists: ${userInfo.publicGists}`;
+    userFollowers.innerHTML = `Followers: ${userInfo.followers}`;
+    userFollowing.innerHTML = `Following: ${userInfo.following}`;
+  }
+
+  updateUserDetails(userInfo) {
+    const userCompany = document.getElementsByClassName('info1')[0];
+    const userWebBlog = document.getElementsByClassName('info2')[0];
+    const userLocation = document.getElementsByClassName('info3')[0];
+    const userMemberSince = document.getElementsByClassName('info4')[0];
+
+    userCompany.innerHTML = userInfo.company
+      ? `Company: ${userInfo.company}`
+      : '<p>Company: 등록된 정보가 없습니다.</p>';
+    userWebBlog.innerHTML = userInfo.webBlog
+      ? `Website & Blog: <a href="${userInfo.webBlog}" target="_blank">${userInfo.webBlog}</a>`
+      : '<p>Website & Blog: 등록된 정보가 없습니다.</p>';
+    userLocation.innerHTML = userInfo.location
+      ? `Location: ${userInfo.location}`
+      : '<p>Location: 등록된 정보가 없습니다.</p>';
+    userMemberSince.innerHTML = `Member Since: ${
+      userInfo.memberSince ? new Date(Date.parse(userInfo.memberSince)).toDateString() : 'Unknown'
+    }`;
+  }
+
+  updateUserGhchart(id) {
+    const ghchartImage = document.getElementById('ghchartImg');
+    this.ghchartForm.classList.remove('hidden');
+    ghchartImage.src = `https://ghchart.rshah.org/${id}`;
+  }
+
+  updateUserRepo(repoInfos) {
+    const repoContainer = document.getElementById('repoContainer');
+    if (!repoContainer) {
+      return; // repoContainer가 없으면 함수 종료
+    }
+    const template = `
+      <div class="repoBox borderBox flexRow">
         <div class="repoNameText">${repoInfos.repoNames}</div>
         <div class="tags flexRow">
           <div class="blue">Stars: ${repoInfos.repoStars}</div>
           <div class="grey">Watchers: ${repoInfos.repoWatchers}</div>
           <div class="green">Forks: ${repoInfos.repoForks}</div>
         </div>
-  </div>
-  `;
-
-  repoContainer.insertAdjacentHTML('beforeend', template);
+      </div>
+    `;
+    repoContainer.insertAdjacentHTML('beforeend', template);
+  }
 }
+
+const githubUser = new GithubUser();
