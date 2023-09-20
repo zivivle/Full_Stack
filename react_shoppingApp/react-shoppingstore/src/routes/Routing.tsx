3ada41b32ce94f9527d464bb29e2dcfd6ecb627e
zivivle <zivivle@gmail.com>
2023-09-20 12:56:17 +0900

    fix: routing.ts -> routing.tsx로 파일명 변경 오류 해결

--- react_shoppingApp/react-shoppingstore/src/routes/Routing.tsx
+++ react_shoppingApp/react-shoppingstore/src/routes/Routing.tsx
@@ -6,15 +6,8 @@ import LoginPage from "../pages/login";
 import SignUpPage from "../pages/signup";
 import ProductDetail from "../pages/product-detail";
 import ProtectedRoute from "./ProtectedRoute";
-import { ReactNode } from "react";
 
-interface RouteConfig {
-  path?: string;
-  element?: ReactNode;
-  children?: RouteConfig[]; // 자식 라우트 설정을 재귀적으로 참조
-}
-
-const router: RouteConfig[] = createBrowserRouter([
+const router = createBrowserRouter([
   {
     element: <Layout />,
     children: [
